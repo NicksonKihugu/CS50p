fc3983a9b5f47f16b060846ee4a4e4417528c824
Nickson Kihugu <nicksonkihugu1@gmail.com>
2026-01-19 00:59:37 +0300

    Adding pset5 solutions

--- pset5/bitcoin.py
+++ pset5/bitcoin.py
@@ -1,17 +1,17 @@
 import requests
 import sys
 
-# 1. Check command-line arguments
+#  Check command-line arguments
 if len(sys.argv) != 2:
     sys.exit("Missing command-line argument")
 
-# 2. Convert to float
+#  Convert to float
 try:
     bitcoins = float(sys.argv[1])
 except ValueError:
     sys.exit("Command-line argument is not a number")
 
-# 3. Get Bitcoin price
+#  Get Bitcoin price
 response = requests.get(
     "https://api.coingecko.com/api/v3/simple/price",
     params={"ids": "bitcoin", "vs_currencies": "usd"}
@@ -21,7 +21,7 @@ data = response.json()
 price = data["bitcoin"]["usd"]
 
 
-# 4. Calculate and print
+#  Calculate and print
 total = bitcoins * price
 print(f"${total:,.4f}")
 
